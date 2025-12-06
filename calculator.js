5430a11105e89e504425dd83c139d4ed17cb5282
Prince Kumar Vishwakarma <princeisonline4u@gmail.com>
2025-12-06 12:00:52 +0530

    Add calculator project with HTML, CSS and JavaScript

---
+++ calculator.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('result');
+
+function appendToDisplay(value) {
+    if (display.value === '0' || display.value === 'Error') {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = '0';
+}
+
+function deleteLast() {
+    if (display.value.length > 1) {
+        display.value = display.value.slice(0, -1);
+    } else {
+        display.value = '0';
+    }
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
